34b32eecf1c8ca59fdd07432e683a48a492c4bd5
AmanMuricken <89331271+AmanMuricken@users.noreply.github.com>
2021-09-05 10:45:38 +0530

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,8 @@
+import random
+when = ["Once upon a time","last night","on 15th august"]
+who =  ["rabbit","tappu","tiger"]
+name = ["roy","thambi","lilly"]
+place = ["lake","pond","sea"]
+went = ["china","uganda","pacific ocean"]
+happened = ["danced","killed","hide and seek"]
+print(random.choice(when),",there lived a",random.choice(who),"named",random.choice(name),"and he lived under a",random.choice(place),". One day he went to",random.choice(went),"and he",random.choice(happened))
